8e4e2326f5378bfc4f264065a03a2707e0c1a75b
mosclov <federico@grupofemal.com>
2017-12-19 00:17:07 -0800

    safex to profit

--- app/controllers/cryptos_controller.rb
+++ app/controllers/cryptos_controller.rb
@@ -10,7 +10,7 @@ class CryptosController < ApplicationController
     @result = JSON.parse(url.body)
     @investment = 3825.0
     total
-    @profit = @total - @investment
+    @profit = (@total + 520) - @investment
     percentage = Change.last
     @daily = percentage.percentage.to_s
   end
