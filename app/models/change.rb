e6aa65e0e5afaf1af299f2a39af830b79a79cca6
mosclov <federico@grupofemal.com>
2017-12-26 09:46:32 -0800

    last day limit change

--- app/models/change.rb
+++ app/models/change.rb
@@ -5,7 +5,7 @@ class Change < ApplicationRecord
     cryptos = Crypto.all.where.not(name: nil, day: true)
     @price = []
     cryptos.each do |c|
-      url = HTTParty.get("https://api.coinmarketcap.com/v1/ticker/?start=0&limit=250")
+      url = HTTParty.get("https://api.coinmarketcap.com/v1/ticker/?start=0&limit=1250")
       coin = JSON.parse(url.body)
       coin.each do |x|
         if x["id"] == c.name
